538b77c6c7e36ce7753e517530f41fe36ff6185c
Adam Lewenhaupt <adam.lewenhauptt@gmail.com>
2012-11-21 19:12:45 +0100

    readded implification

--- src/AL/Compile.hs
+++ src/AL/Compile.hs
@@ -33,18 +33,30 @@ compileClause = foldl' constructDB db
 constructDB :: Database -> Rule -> Database
 constructDB db@(Database m) rule = case rule of
 	(Relation n xs) -> Database $ M.insertWith union n [xs] m
+	(Imply (Relation n xs) r2) -> Database $ M.insertWith union n ( 
+		impliedMarkedEntries db xs n $ evalRule db r2) m
 	_ -> db
+
+
+evalRule :: Database -> Rule -> [(String, String)]
+evalRule db (Relation n xs) = getEs db xs n
+evalRule db (And r1 r2) = commonMarkedEntries (evalRule db r1) (evalRule db r2)
 
 
 -- 
 -- Compiler operations.
 --
-commonMarkedEntries :: Database -> [String] -> [String] -> String -> String -> [(String, String)]
-commonMarkedEntries db vars1 vars2 q1 q2 = uncurry intersect $ getCmpEntries db vars1 vars2 q1 q2
+commonMarkedEntries :: [(String, String)] -> [(String, String)] -> [(String, String)]
+commonMarkedEntries = intersect
 
 
 impliedMarkedEntries :: Database -> [String] -> String -> [(String, String)] -> [[String]]
-impliedMarkedEntries db tvars tar svars = undefined
+impliedMarkedEntries db tvars tar svars = reverse $ foldl' go [] tvars
+	where
+		go :: [[String]] -> String -> [[String]]
+		go a b
+			| isUpper $ head b = map snd (filter ((==b).fst) svars):a
+			| otherwise = [b]:a
 --
 -- End of compiler operations.
 --
@@ -53,9 +65,11 @@ impliedMarkedEntries db tvars tar svars = undefined
 getCmpEntries :: Database -> [String] -> [String] -> String -> String -> ([(String, String)], [(String, String)])
 getCmpEntries db vars1 vars2 q1 q2 = (es1, es2)
 		where
-			getEs = \vars -> filter ((`elem`vars) . fst) . concat . getEntriesMarked db vars
-			es1 = getEs vars1 q1
-			es2 = getEs vars2 q2
+			es1 = getEs db vars1 q1
+			es2 = getEs db vars2 q2
+
+getEs :: Database -> [String] -> String -> [(String, String)]
+getEs db vars = filter ((`elem`vars) . fst) . concat . getEntriesMarked db vars
 
 getEntriesMarked :: Database -> [String] -> String -> [[(String, String)]]
 getEntriesMarked db@(Database m) vars query = evalVariables vars $ getEntries db query
@@ -74,6 +88,13 @@ evalVariables vars rs = filter ((==length (head rs)).length) $ map (go vars) rs
 		go (v:vs) (r:rs) = if isUpper (head v) then (v, r):go vs rs
 							else if v == r then ("_", r): go vs rs
 							else []
+
+
+combinator :: [a] -> [a] -> [[a]]
+combinator first second = go first
+	where
+		go [] = []
+		go (x:xs) = (x:second):go xs
 
 
 debug :: IO Database

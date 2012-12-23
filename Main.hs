f937f7ade28c1ebe79e864b84b309a74b68d89ed
Adam Lewenhaupt <adam.lewenhauptt@gmail.com>
2012-12-23 17:33:01 +0100

    done v1.0

--- Main.hs
+++ Main.hs
@@ -4,7 +4,8 @@ import AL
 
 main :: IO ()
 main = do
+	let items = ["jealous", "happy", "unhappy"]
 	d <- readFile "test.txt"
 	case compile d of
 		Nothing -> return ()
-		Just x -> print (query x "jealous")
+		Just x -> mapM_ putStrLn $ map (\a -> ((a++": ")++) $ show $ query x a) items
